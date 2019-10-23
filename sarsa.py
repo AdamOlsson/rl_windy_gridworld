56fe218a8db2f4a0d1a10487794794a1a7b2f73f
AdamOlsson <Olsson.95_@hotmail.com>
2019-10-23 15:09:52 +0200

    Fixed bug causing action for next state was based on previous state

--- sarsa.py
+++ sarsa.py
@@ -4,50 +4,107 @@ from matplotlib.animation import FuncAnimation
 from windy_gridworld import WindyGridworld
 from collections import defaultdict
 
-ACT_TO_IND = {[1,0]:0, [-1,0]:1, [0,1]:2, [0,-1]:3}
-IND_TO_ACT = {0:[1,0], 1:[-1,0], 2:[0,1], 3:[0,-1]}
+ACT_TO_IND = {(1,0):0, (-1,0):1, (0,1):2, (0,-1):3}
+IND_TO_ACT = {0:(1,0), 1:(-1,0), 2:(0,1), 3:(0,-1)}
 
 def get_q_state(Q, state, env):
-    return lambda a : [Q[state, action] for action in np.arange(env.action_space)][a]
+    return lambda a : [Q[state,action] for action in np.arange(env.action_space)][a]
 
-def sarsa(env, policy, epsilon=0.1, alpha=0.5, gamma=1, iterations=10):
+'''Function only handles reading from Q, not writing. Removes the need to call ACT_TO_IND[x] on every Q read'''
+def create_q_reader(Q):
+    return lambda s, a : Q[s, ACT_TO_IND[a]]
+
+
+def sarsa(env, policy, epsilon=0.1, alpha=0.5, gamma=1, iterations=1000):
 
     Q = defaultdict(float)
-    
+    Q_reader = create_q_reader(Q)
+    no_steps = 0
+
     for episode in range(iterations):
+        if episode % 10 == 0:
+            print("Playing episode {} out of {}. Average number of steps: {}.".format(episode, iterations, no_steps/10))
+            no_steps = 0
+
         state = env.reset()
-        action = policy(state)
-        
+        action = policy(epsilon, env.get_actions(state), get_q_state(Q, state, env))
+
         game_over = False
         while not game_over:
             next_state, reward, game_over, info = env.step(action)
-            next_action = policy(epsilon, env.get_actions(state), get_q_state(Q, state, env))
+            next_action = policy(epsilon, env.get_actions(next_state), get_q_state(Q, state, env))
 
-            Q[state, action] += alpha*(reward + gamma*Q[next_state, next_action] - Q[state, action])
+            Q[state, ACT_TO_IND[action]] += alpha*(reward + gamma*Q_reader(next_state, next_action) - Q_reader(state, action))
 
             state = next_state; action = next_action
+            no_steps += 1
             
     return Q
 
-# TODO
+
 def behavior_policy(epsilon, actions, q_state_values):
+    '''
+    Select the a greedy action with prob 1-epsilon. Select a non-greedy action with prob epsilon
+    '''
+    action_ids = [ACT_TO_IND[a] for a in actions]            # index of actions
+    state_values = np.array([q_state_values(i) for i in action_ids])   # state value of actions
 
-    action_id = [ACT_TO_IND[a] for a in actions]            # index of actions
-    state_values = [q_state_values(i) for i in action_id]   # state value of actions
+    best_action     = np.random.choice([action_ids[i] for i in np.where(state_values == state_values.max())[0]]) # select all greedy actions
+    random_action   = np.random.choice([action_ids[i] for i in np.where(action_ids != best_action)[0]]) # select non-greedy action
 
-    # TODO: Find best action
+    # Explore or greedy action?
+    action_id = np.random.choice([best_action, random_action], p=[1-epsilon, epsilon])
 
-    if np.random.random_sample() < epsilon:
-        # Take a random action from the set of non-greedy actions
-        pass
-    else:
-        # Take a greedy action
-        pass
+    return IND_TO_ACT[action_id]
 
+def get_policy(Q, shape):
+    greedy_actions = np.zeros(shape)
 
-def draw_Q(Q):
-    #TODO
-    pass
+    for (y,x), a in Q.keys():
+        if greedy_actions[y,x] == 0 or greedy_actions[y,x] < Q[(y,x), a] and not Q[(y,x), a] == 0:
+            greedy_actions[y,x] = a
+    
+    return greedy_actions
+
+def draw_Q(Q, shape):
+
+    best_q_values = np.zeros(shape)
+
+    for (y,x), a in Q.keys():
+        if best_q_values[y,x] == 0 or best_q_values[y,x] < Q[(y,x), a] and not Q[(y,x), a] == 0:
+            best_q_values[y,x] = Q[(y,x), a]
+    
+    fig, ax = plt.subplots(figsize=(20,15))
+    plt.title('State-Action Function', fontsize=30)
+
+    ax.axes.xaxis.set_ticklabels([])
+    ax.axes.yaxis.set_ticklabels([])
+    ax.axes.xaxis.set_ticks([])
+    ax.axes.yaxis.set_ticks([])
+
+    im = ax.imshow(best_q_values)
+
+    cbar = ax.figure.colorbar(im, ax=ax)
+    cbar.ax.tick_params(labelsize=20)
+    cbar.ax.set_ylabel('Value', rotation=-90, va='bottom', fontsize=20)
+
+    plt.show()
+    
+
+# TODO: Verify this and plot policy
+def play(env, policy):
+    history = []
+
+    state = env.reset()
+    game_over = False
+
+    while not game_over:
+        history.append(state)
+        action = policy[state[0], state[1]]
+        next_state, reward, game_over, info = env.step(action)
+        state = next_state
+
+    return history
 
 def animate_Q(env, Q):
     # TODO
@@ -81,4 +138,6 @@ def animate_Q(env, Q):
 
 if __name__ == "__main__":
     env = WindyGridworld()
-    sarsa(env, behavior_policy)
+    Q = sarsa(env, behavior_policy, iterations=10)
+
+    draw_Q(Q, env.state_space)
--- windy_gridworld.py
+++ windy_gridworld.py
@@ -6,20 +6,24 @@ class WindyGridworld():
         self.state_space = (7,10)
         self.action_space = 4
 
+        self.action_list = [(1,0), (-1,0), (0,1), (0,-1)]
+
         #self.grid = np.zeros(self.state_space)
 
-        self.initial_state = [3,0]
-        self.terminate_state = [3,8]
+        self.initial_state = (3,0)
+        self.terminate_state = (3,7)
 
-        self.north_wind = [0,0,0,1,1,1,2,2,1,0] # how many cells respective column will push agent north
+        self.north_wind = [0,0,0,-1,-1,-1,-2,-2,-1,0] # how many cells respective column will push agent north
 
         self.pos = None
 
 
     def step(self, action):
 
-        self.pos[0] = min(max(self.pos[0] + self.north_wind[self.pos[0]] + action[0], 0), self.state_space[0]-1) # adding wind as well
-        self.pos[1] = min(max(self.pos[1] + action[1], 0), self.state_space[1]-1)
+        pos0 = min(max(self.pos[0] + self.north_wind[self.pos[1]] + action[0], 0), self.state_space[0]-1) # adding wind as well
+        pos1 = min(max(self.pos[1] + action[1], 0), self.state_space[1]-1)
+
+        self.pos = (pos0, pos1)
 
         if self.pos == self.terminate_state:
             game_over = True
@@ -37,3 +41,18 @@ class WindyGridworld():
         self.pos = self.initial_state
 
         return self.pos
+
+    # Returns the set of available action for a given state
+    def get_actions(self, state):
+        actions = []
+
+        if 0 <= state[0] -1:
+            actions.append((-1,0))
+        if state[0] +1 < self.state_space[0]:
+            actions.append((1,0))
+        if 0 <= state[1] -1:
+            actions.append((0,-1))
+        if state[1] +1 < self.state_space[1]:
+            actions.append((0,1))
+        
+        return actions
